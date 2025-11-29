56925c210e2381d27439ecbf98ef1b7a53847af4
Jamie Blättler <jamie.blaettler@proton.me>
2025-11-29 10:43:17 +0100

    Setup cli (#1)

---
+++ src/year2015/mod.rs
@@ -0,0 +1,27 @@
+use std::fs;
+
+use crate::AocError;
+
+mod day01;
+
+pub fn run(day: u8, part: u8) -> Result<String, AocError> {
+    match day {
+        1 => match part {
+            1 => Ok(day01::part1(read_file_to_string(day01::INPUT_PATH)).to_string()),
+            2 => Ok(day01::part2(read_file_to_string(day01::INPUT_PATH)).to_string()),
+            _ => Err(AocError::PartNotFound {
+                year: 2015,
+                day,
+                part,
+            }),
+        },
+        _ => Err(AocError::DayNotFound { year: 2015, day }),
+    }
+}
+
+fn read_file_to_string(path: &str) -> String {
+    match fs::read_to_string(path) {
+        Ok(s) => s,
+        Err(e) => panic!("{e}"),
+    }
+}
