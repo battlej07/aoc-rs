3cbdc7c4cb1bf6fa92112527bde0f4d10e9418e6
Jamie Blättler <jamie.blaettler@proton.me>
2025-11-29 22:21:57 +0100

    Day 01: Solve part 2

--- src/year2015/day01.rs
+++ src/year2015/day01.rs
@@ -14,14 +14,27 @@ pub fn part1(input: String) -> i64 {
     level
 }
 
-#[allow(unused_variables)]
 pub fn part2(input: String) -> i64 {
-    todo!()
+    let mut level = 0;
+
+    for (i, c) in input.char_indices() {
+        if c == '(' {
+            level += 1;
+        } else if c == ')' {
+            level -= 1
+        }
+
+        if level == -1 {
+            return i as i64 + 1;
+        }
+    }
+
+    0
 }
 
 #[cfg(test)]
 mod test {
-    use crate::year2015::day01::part1;
+    use crate::year2015::day01::{part1, part2};
 
     #[test]
     fn part1_examples() {
@@ -34,5 +47,11 @@ mod test {
         assert_eq!(-1, part1("))(".to_string()));
         assert_eq!(-3, part1(")))".to_string()));
         assert_eq!(-3, part1(")())())".to_string()));
+    }
+
+    #[test]
+    fn part2_examples() {
+        assert_eq!(1, part2(")".to_string()));
+        assert_eq!(5, part2("()())".to_string()));
     }
 }
