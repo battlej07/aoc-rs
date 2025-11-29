b490573fe11b3d1eeac2039fa6e3be772db79b57
Jamie Blättler <jamie.blaettler@proton.me>
2025-11-29 10:43:17 +0100

    Setup cli (#1)

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+use core::fmt;
+use std::error::Error;
+
+mod year2025;
+
+#[derive(Debug, Clone)]
+pub enum AocError {
+    YearNotFound { year: u16 },
+    DayNotFound { year: u16, day: u8 },
+    PartNotFound { year: u16, day: u8, part: u8 },
+}
+
+impl fmt::Display for AocError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match *self {
+            Self::YearNotFound { year } => write!(f, "Year {year} not found"),
+            Self::DayNotFound { year, day } => write!(f, "Day {day} not found in year {year}"),
+            Self::PartNotFound { year, day, part } => {
+                write!(f, "Part {part} not found in day {day} of year {year}")
+            }
+        }
+    }
+}
+
+impl Error for AocError {}
+
+pub fn run(year: u16, day: u8, part: u8) -> Result<String, AocError> {
+    match year {
+        2015 => year2025::run(day, part),
+        other => Err(AocError::YearNotFound { year: other }),
+    }
+}
---
+++ src/year2025/mod.rs
@@ -0,0 +1,28 @@
+use std::fs;
+
+use crate::AocError;
+
+mod day01;
+
+pub fn run(day: u8, part: u8) -> Result<String, AocError> {
+    match day {
+        1 => match part {
+            1 => Ok(day01::part1(read_file_to_string(day01::INPUT_PATH)).to_string()),
+            2 => Ok(day01::part2(read_file_to_string(day01::INPUT_PATH)).to_string()),
+            _ => Err(AocError::PartNotFound {
+                year: 2015,
+                day,
+                part,
+            }),
+        },
+        _ => Err(AocError::DayNotFound { year: 2015, day }),
+    }
+}
+
+fn read_file_to_string(path: &str) -> String {
+    if let Ok(s) = fs::read_to_string(path) {
+        s
+    } else {
+        "".to_string()
+    }
+}
