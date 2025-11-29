2f01106c6ace691255019285864d5a912a743b57
Jamie Blättler <jamie.blaettler@proton.me>
2025-11-29 21:52:18 +0100

    Day 01: Solve part 1

--- src/year2015/day01.rs
+++ src/year2015/day01.rs
@@ -1,11 +1,38 @@
-pub const INPUT_PATH: &str = "../../input/2015/day01.txt";
+pub const INPUT_PATH: &str = "input/2015/day01.txt";
 
-#[allow(unused_variables)]
 pub fn part1(input: String) -> i64 {
-    todo!()
+    let mut level = 0;
+
+    for c in input.chars() {
+        if c == '(' {
+            level += 1;
+        } else if c == ')' {
+            level -= 1;
+        }
+    }
+
+    level
 }
 
 #[allow(unused_variables)]
 pub fn part2(input: String) -> i64 {
     todo!()
+}
+
+#[cfg(test)]
+mod test {
+    use crate::year2015::day01::part1;
+
+    #[test]
+    fn part1_examples() {
+        assert_eq!(0, part1("(())".to_string()));
+        assert_eq!(0, part1("()()".to_string()));
+        assert_eq!(3, part1("(((".to_string()));
+        assert_eq!(3, part1("(()(()(".to_string()));
+        assert_eq!(3, part1("))(((((".to_string()));
+        assert_eq!(-1, part1("())".to_string()));
+        assert_eq!(-1, part1("))(".to_string()));
+        assert_eq!(-3, part1(")))".to_string()));
+        assert_eq!(-3, part1(")())())".to_string()));
+    }
 }
