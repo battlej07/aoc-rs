9bf0eb47acabae5b967d445f546f5dbf68e45c09
Jamie Blättler <jamie.blaettler@proton.me>
2025-11-30 11:10:50 +0100

    Day 02: Setup infrastrucure

--- src/year2015/mod.rs
+++ src/year2015/mod.rs
@@ -3,12 +3,22 @@ use std::fs;
 use crate::AocError;
 
 mod day01;
+mod day02;
 
 pub fn run(day: u8, part: u8) -> Result<String, AocError> {
     match day {
         1 => match part {
             1 => Ok(day01::part1(read_file_to_string(day01::INPUT_PATH)).to_string()),
             2 => Ok(day01::part2(read_file_to_string(day01::INPUT_PATH)).to_string()),
+            _ => Err(AocError::PartNotFound {
+                year: 2015,
+                day,
+                part,
+            }),
+        },
+        2 => match part {
+            1 => Ok(day02::part1(read_file_to_string(day02::INPUT_PATH)).to_string()),
+            2 => Ok(day02::part2(read_file_to_string(day02::INPUT_PATH)).to_string()),
             _ => Err(AocError::PartNotFound {
                 year: 2015,
                 day,
