77fd1b2a9d24bd9da366113f6d3f9dca0ece5129
Jamie Blättler <jamie.blaettler@proton.me>
2025-12-06 10:54:47 +0100

    Day 03: Setup

--- src/year2025/mod.rs
+++ src/year2025/mod.rs
@@ -4,6 +4,7 @@ use crate::AocError;
 
 mod day01;
 mod day02;
+mod day03;
 
 pub fn run(day: u8, part: u8) -> Result<String, AocError> {
     match day {
@@ -19,6 +20,15 @@ pub fn run(day: u8, part: u8) -> Result<String, AocError> {
         2 => match part {
             1 => Ok(day02::part1(read_file_to_string(day02::INPUT_PATH)).to_string()),
             2 => Ok(day02::part2(read_file_to_string(day02::INPUT_PATH)).to_string()),
+            _ => Err(AocError::PartNotFound {
+                year: 2025,
+                day,
+                part,
+            }),
+        },
+        3 => match part {
+            1 => Ok(day03::part1(read_file_to_string(day03::INPUT_PATH)).to_string()),
+            2 => Ok(day03::part2(read_file_to_string(day03::INPUT_PATH)).to_string()),
             _ => Err(AocError::PartNotFound {
                 year: 2025,
                 day,
