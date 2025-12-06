1f598c65d03ea617151efa5586d583cb8b03e0e4
Jamie Blättler <jamie.blaettler@proton.me>
2025-12-06 10:54:47 +0100

    Day 03: Setup

---
+++ src/year2025/day03.rs
@@ -0,0 +1,22 @@
+pub const INPUT_PATH: &str = "input/2025/day03.txt";
+
+pub fn part1(input: String) -> u32 {
+    todo!()
+}
+
+pub fn part2(input: String) -> u32 {
+    todo!()
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn part1_example() {
+        todo!();
+    }
+
+    #[test]
+    fn part2_example() {
+        todo!();
+    }
+}
