d0e6f3fd91c70fdaeb65512d4b6a69991adbc3e7
Jamie Blättler <jamie.blaettler@proton.me>
2025-12-07 20:38:46 +0100

    Day 03: Solve part 02

--- src/year2025/day03.rs
+++ src/year2025/day03.rs
@@ -25,42 +25,61 @@ pub fn part1(input: String) -> u32 {
             best
         })
         .sum()
-
-    // let mut sum = 0;
-    //
-    // for bank in banks {
-    //     let mut x: Vec<(usize, u32)> = bank
-    //         .trim()
-    //         .char_indices()
-    //         .map(|(i, c)| (i, c.to_string().parse::<u32>().unwrap()))
-    //         .collect();
-    //
-    //     x.sort_by(|a, b| a.1.cmp(&b.1));
-    //
-    //     let a = x[x.len() - 1];
-    //     let b = x[x.len() - 2];
-    //
-    //     println!("Bank: {bank}");
-    //     println!("Sorted: {:?}", x);
-    //     println!("A: {:?}; B: {:?}", a, b);
-    //
-    //     sum += if a.0 < b.0 {
-    //         format!("{}{}", a.1, b.1).parse::<u32>().unwrap()
-    //     } else {
-    //         format!("{}{}", b.1, a.1).parse::<u32>().unwrap()
-    //     }
-    // }
-    //
-    // sum
 }
 
-pub fn part2(input: String) -> u32 {
-    todo!()
+pub fn part2(input: String) -> u64 {
+    input
+        .lines()
+        .map(|bank| {
+            let digites: Vec<u64> = bank
+                .trim()
+                .chars()
+                .map(|c| c.to_digit(10).unwrap() as u64)
+                .collect();
+
+            let mut best = 0;
+            let l = digites.len();
+
+            let mut needed = 12;
+            let mut position = 0;
+
+            while needed > 0 {
+                let max_pos = l - needed;
+
+                let available_values = &digites[position..=max_pos];
+                if let Some(best_value) = first_max(available_values) {
+                    best = 10 * best + best_value.1;
+
+                    needed -= 1;
+                    position += best_value.0 + 1
+                } else {
+                    break;
+                }
+            }
+
+            best
+        })
+        .sum()
+}
+
+fn first_max(arr: &[u64]) -> Option<(usize, u64)> {
+    if arr.is_empty() {
+        return None;
+    }
+
+    let mut max_index = 0;
+    for (i, &v) in arr.iter().enumerate() {
+        if v > arr[max_index] {
+            max_index = i;
+        }
+    }
+
+    Some((max_index, arr[max_index]))
 }
 
 #[cfg(test)]
 mod test {
-    use crate::year2025::day03::part1;
+    use crate::year2025::day03::{part1, part2};
 
     #[test]
     fn part1_example() {
@@ -75,6 +94,12 @@ mod test {
 
     #[test]
     fn part2_example() {
-        todo!();
+        let input = r#"987654321111111
+811111111111119
+234234234234278
+818181911112111"#
+            .to_string();
+
+        assert_eq!(3121910778619, part2(input));
     }
 }
